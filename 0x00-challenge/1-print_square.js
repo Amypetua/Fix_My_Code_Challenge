4ded457a4710234b24b352eb27f4b356ebf320ed
Amypetua <anachusichiamaka32@gmail.com>
2023-05-27 03:44:23 -0700

    Print a square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -11,18 +11,13 @@ if (process.argv.length <= 2){
 	process.stderr.write("Missing argument\n");
 	process.stderr.write("Usage: ./1-print_square.js <size>\n");
 	process.stderr.write("Example: ./1-print_square.js 8\n");
-	process.exit(1);
+	process.exit(1)
 }
 
-size = parseInt(process.argv[2]);
+size = parseInt(process.argv[2], 10)
 
-if (isNaN(size)) {
-	process.stderr.write("Invalid size\n");
-	process.exit(1);
-}
-
-for (let i = 0; i < size; i++) {
-	for (let j = 0; j < size; j++) {
+for (let i = 0 ; i < size ; i ++) {
+	for (let j = 0 ; j < size ; j ++) {
 		process.stdout.write("#");
 	}
 	process.stdout.write("\n");
